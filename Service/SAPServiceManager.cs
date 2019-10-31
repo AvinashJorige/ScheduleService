da763e4d0bb36d5662ab502817eb54e5c503d345
Avinash Jorige <jo.avi.1990@gmail.com>
2019-10-31 23:35:29 +0530

    SAP Service Completed adding generic method

---
+++ Service/SAPServiceManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Xml;
+using Utility;
+using Newtonsoft;
+
+namespace Service
+{
+    public class SAPServiceManager<T> where T : class
+    {
+         public object ConnectSAPService(List<T> input)
+        {
+            long TimeTookticks          = DateTime.Now.Ticks;
+            XmlNode xmlExceptionLocStat = null;
+            SAPService _SAPCon          = null;
+            XmlElement xDocTableLocStat = null;
+            XmlDocument xDocLocStat     = null;
+            DataTable _datatable        = new DataTable();
+
+            try
+            {
+                Log4net.LogWriter("SAPService", "Service", "ConnectSAPService || Started : " + Tools.TicksToTime(DateTime.Now.Ticks), Entities.LogType.LogMode.Debug);
+
+                _SAPCon          = new SAPService(AppSettingConfig.GetConfigAppSetting("SAPWebServiceURL"));
+                xDocLocStat      = XMLSerializer.Serialize(input, XMLSerializer.InputType.List);
+                xDocTableLocStat = (XmlElement)_SAPCon.GetTableData(xDocLocStat, out xmlExceptionLocStat);
+
+                List<T> SAPErrorLocStat = (List<T>)XMLSerializer.Deserialize(xmlExceptionLocStat.OuterXml, XMLSerializer.OutputType.List);
+
+                if (SAPErrorLocStat != null && SAPErrorLocStat.Count > 0 && SAPErrorLocStat[0] != null && SAPErrorLocStat[0].ToString() == "1000")
+                {
+                    _datatable = (DataTable)XMLSerializer.Deserialize(xDocTableLocStat.OuterXml, XMLSerializer.OutputType.DataTable);
+                }
+
+                Log4net.LogWriter("SAPService", "Service", "ConnectSAPService || Resultset Datatable : " + Newtonsoft.Json.JsonConvert.SerializeObject(_datatable), Entities.LogType.LogMode.Debug);
+                Log4net.LogWriter("SAPService", "Service", "ConnectSAPService || Resultset Datatable End: --------------------------------" + Environment.NewLine +""+ Environment.NewLine, Entities.LogType.LogMode.Debug);
+                Log4net.LogWriter("SAPService", "Service", "ConnectSAPService || Service Ended : " + Tools.TicksToTime(TimeTookticks), Entities.LogType.LogMode.Debug);
+                Log4net.LogWriter("SAPService", "Service", "ConnectSAPService || Service Ended: --------------------------------" + Environment.NewLine + "" + Environment.NewLine, Entities.LogType.LogMode.Debug);
+
+                return _datatable;
+            }
+            catch (Exception ex)
+            {
+                Log4net.LogWriter("SAPService", "Service", "ConnectSAPService || Error : " + ex.Message, Entities.LogType.LogMode.Error);
+            }
+            finally
+            {
+                _datatable          = null;
+                _SAPCon             = null;
+                xDocLocStat         = null;
+                xDocTableLocStat    = null;
+                xmlExceptionLocStat = null;
+            }
+            return null;
+        }
+    }
+}
--- Utility/AppSettingConfig.cs
+++ Utility/AppSettingConfig.cs
@@ -1,16 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Utility
 {
-    public class AppSettingConfig
+    public static class AppSettingConfig
     {
-        public AppSettingConfig()
+        public static string GetConfigAppSetting(object key)
         {
-
+            string strResult = string.Empty;
+            try
+            {
+                if(key != null && !string.IsNullOrEmpty(key.ToString()))
+                {
+                    strResult = ConfigurationSettings.AppSettings[key.ToString()];
+                    return Tools.ConvertObjectToString(strResult);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log4net.LogWriter("Utility", "AppSettingConfig", "AppSettingConfig || Error : " + ex.Message, Entities.LogType.LogMode.Error);
+            }
+            return strResult;
         }
     }
 }
